f97b1e73e01bc654895485ecddb9959df4ea3af3
Lawrence Colman <Lawrence.Colman@SKUtopia.com>
2025-10-23 19:03:19 +1100

    update CustomClaimValue type to match JWT and JSON RFCs

--- src/api/types/CustomClaims.d.ts
+++ src/api/types/CustomClaims.d.ts
@@ -1,9 +1,25 @@
 /**
- * Nested object inside custom claim on either the Access Token or the Identity Token.
+ * As defined in Section 2 of RFC 7519 (the JSON Web Token specification):
+ * ```
+ * The value portion of a claim representation.  A Claim Value can be any JSON value.
+ * ```
+ * <https://datatracker.ietf.org/doc/html/rfc7519#section-2>
+ * 
+ * The JavaScript Object Notation (JSON), as defined in RFC 7159, defines a value as one of six types:
+ * ```
+ * A JSON value MUST be an object, array, number, or string, or one of the following three literal names:
+ *       false null true
+ * ```
+ * <https://datatracker.ietf.org/doc/html/rfc7159#section-3>
  */
-export type CustomClaimObject = Record<string, string | string[] | Record<string, string> | Record<string, string[]>>;
-
+type Claim =
+  | { [key: string]: Claim } // object
+  | Claim[]                  // array
+  | number
+  | string
+  | boolean
+  | null
 /**
- * Value of a custom claim on either the Access Token or the Identity Token.
+ * Value of a claim on either the Access Token or the Identity Token.
  */
-export type CustomClaimValue = string | string[] | Record<string, string> | Record<string, string[]> | CustomClaimObject;
+export type CustomClaimValue = Claim;
